95074aaccac7ebb6a74ccdc5f5d5d592af24ad75
Zinn Morton <mortonzi@oregonstate.edu>
2021-03-05 22:15:19 -0800

    Implemented functionality for leap years divisible by 400

--- leapyear.py
+++ leapyear.py
@@ -1,7 +1,10 @@
 def leapyear(year):
     if(year % 4 == 0):
         if(year % 100 == 0):
-            return "is not a leap year"
+            if(year % 400 == 0):
+                return "is a leap year"
+            else:
+                return "is not a leap year"
         else:
             return "is a leap year"
     else:
