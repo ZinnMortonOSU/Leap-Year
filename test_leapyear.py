6c843db592266e61d47cf215b7fdb6050606fc6d
Zinn Morton <mortonzi@oregonstate.edu>
2021-03-05 22:07:44 -0800

    Wrote second test for non leap years which are not divisible by 4

--- test_leapyear.py
+++ test_leapyear.py
@@ -3,6 +3,11 @@ import leapyear
 
 class TestLeapYear(unittest.TestCase):
     def test_LeapYear(self):
+        #Non leap years not divisible by 4
+        self.assertEqual(leapyear.leapyear(7), "is not a leap year")
+        self.assertEqual(leapyear.leapyear(83), "is not a leap year")
+        self.assertEqual(leapyear.leapyear(101), "is not a leap year")
+
         #Years divisble by 4 but not 100
         self.assertEqual(leapyear.leapyear(4), "is a leap year")
         self.assertEqual(leapyear.leapyear(16), "is a leap year")
